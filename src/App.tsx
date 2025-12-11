dea42a8bfcef18160f23d30b17f14b71d9871744
Ecs1d <egorgorynyc033@gmail.com>
2025-12-11 19:13:00 +0300

    init2

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,8 @@ import PersonSearchIcon from '@mui/icons-material/PersonSearch'
 import UserProfile from './components/UserProfile/UserProfile'
 import SearchUsers from './components/SearchUsers/SearchUsers'
 
-const tg = window.Telegram.WebApp
-
 function App() {
 	const [value, setValue] = React.useState(0)
-
-	React.useEffect(() => {
-		tg.ready()
-	}, [])
 
 	return (
 		<>
