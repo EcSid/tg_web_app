3dc2c317d39f8c9a4ce27d4b4e0c479fd6e9d36c
Ecs1d <egorgorynyc033@gmail.com>
2025-12-11 19:19:04 +0300

    init4

--- src/components/VirtualizedListUsers/VirtualizedListUsers.tsx
+++ src/components/VirtualizedListUsers/VirtualizedListUsers.tsx
@@ -4,7 +4,7 @@ import ListItemButton from '@mui/material/ListItemButton'
 import ListItemText from '@mui/material/ListItemText'
 import ListItemAvatar from '@mui/material/ListItemAvatar'
 import Avatar from '@mui/material/Avatar'
-import { Button, IconButton } from '@mui/material'
+import { IconButton } from '@mui/material'
 import MessageIcon from '@mui/icons-material/Message'
 
 const VurtualizedListUsers = () => {
